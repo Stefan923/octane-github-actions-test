f547e2c06d5660faa9835563fd3ea18852bb332e
Ovidiu Stefan.Popescu.76011119 <opopescu@opentext.com>
2024-11-28 20:41:34 +0200

    Update JUnit tests - add failed and skipped tests

---
+++ src/test/java/me/opopescu/test_octane_github_actions/BooleanCalculatorTest.java
@@ -0,0 +1,86 @@
+package me.opopescu.test_octane_github_actions;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+public class BooleanCalculatorTest {
+    private boolean isFirstTestPassed = false;
+
+    @DisplayName("Or test for one true")
+    @Test
+    public void testOr_whenOneTrue() {
+        boolean expectedResult = true;
+
+        BooleanCalculator calc = new BooleanCalculator();
+        calc.or(true, false);
+        boolean actualResult = calc.getResult();
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @DisplayName("And test for one false - failing test")
+    @Test
+    public void testAnd_whenOneFalse_Failing() {
+        boolean expectedResult = true;
+
+        BooleanCalculator calc = new BooleanCalculator();
+        calc.and(false, true);
+        boolean actualResult = calc.getResult();
+
+        assertEquals(expectedResult, actualResult);
+        isFirstTestPassed = false;
+    }
+
+    @DisplayName("Or test for both false")
+    @Test
+    public void testOr_whenBothFalse() {
+        boolean expectedResult = false;
+
+        BooleanCalculator calc = new BooleanCalculator();
+        calc.or(false, false);
+        boolean actualResult = calc.getResult();
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @DisplayName("And test for both true")
+    @Test
+    public void testAnd_whenBothTrue() {
+        boolean expectedResult = true;
+
+        BooleanCalculator calc = new BooleanCalculator();
+        calc.and(true, true);
+        boolean actualResult = calc.getResult();
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @DisplayName("And test for one false")
+    @Test
+    public void testAnd_whenOneFalse() {
+        boolean expectedResult = false;
+
+        BooleanCalculator calc = new BooleanCalculator();
+        calc.and(false, true);
+        boolean actualResult = calc.getResult();
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @DisplayName("Test that should be skipped")
+    @Test
+    public void testSkipped() {
+        assumeTrue(isFirstTestPassed);
+
+        boolean expectedResult = false;
+
+        BooleanCalculator calc = new BooleanCalculator();
+        calc.and(false, true);
+        boolean actualResult = calc.getResult();
+
+        assertEquals(expectedResult, actualResult);
+    }
+}
